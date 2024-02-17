bf031dc6f49dc59ba2481da34b5a9df9f3966fc6
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-02-17 16:49:57 +0900

    Update kkshk.py

--- kkshk.py
+++ kkshk.py
@@ -9,7 +9,7 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 line_parser = WebhookParser(LINE_CHANNEL_SECRET)
 app = FastAPI()
 
-
+username = None
 @app.post('/')
 async def kkshk(request: Request):
     # X-Line-Signature ヘッダーの値を取得
@@ -18,7 +18,7 @@ async def kkshk(request: Request):
     # request body から event オブジェクトを取得
     events = line_parser.parse((await request.body()).decode('utf-8'), signature)
 
-    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェっｚクトが含まれる場合あるため）
+    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェクトが含まれる場合あるため）
     for event in events:
         if event.type != 'message':
             continue
@@ -30,10 +30,14 @@ async def kkshk(request: Request):
         line_message = event.message.text
 
         
-
-        # LINE メッセージの送信
-        line_bot_api.push_message(line_user_id, TextSendMessage("喜びなさい故信田"))
-        line_bot_api.push_message(line_user_id, TextSendMessage(line_message))
+        if username == None:
+            line_bot_api.push_message(line_user_id, TextSendMessage("初めまして！（これは、名前が登録されていない時のメッセージ）"))
+            line_bot_api.push_message(line_user_id, TextSendMessage("あなたが送ったメッセージは「"+line_message+"」です。これをあなたの名前として登録します"))
+            username = line_message
+        else:    
+            line_bot_api.push_message(line_user_id, TextSendMessage("こんにちは、"+username+"さん。"))
+            line_bot_api.push_message(line_user_id, TextSendMessage(line_message+"とはどういう意味でしょうか？"))            
+        
         
     # LINE Webhook サーバーへ HTTP レスポンスを返す
     return 'ok'
