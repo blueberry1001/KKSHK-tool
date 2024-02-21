772113ab4e66f84759d3b228bd031e2e882962cf
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-02-21 13:54:04 +0900

    Update kkshk.py

--- kkshk.py
+++ kkshk.py
@@ -72,24 +72,6 @@ async def kkshk(request: Request):
                 line_bot_api.push_message(line_user_id, TextSendMessage(f"https://docs.google.com/forms/d/e/1FAIpQLSfKyju5yd5Fw08TiEo6Bwe3IO3HTM1gjngvWASKtMR8FpKtuA/viewform?usp=pp_url&entry.998530319={tourl(groupname)}&entry.273281967=2024-02-09&entry.1630282032={tourl(username)}&entry.449921247={tourl(mode)}&entry.1302965683=15:10&entry.2092899857={tourl(place)}&entry.358162710={tourl(teacher)}&entry.841836120={tourl(no)}&entry.1958684523=17:30"))
         
                 
-        #初回登録判定、どうしようか
-        if username != "default-usernam" or True:
-            line_bot_api.push_message(line_user_id, TextSendMessage("初めまして！あなたが送ったメッセージをもとに、空白区切りで情報を登録します。"))
-            x = line_message.split()
-            if len(list(line_message.split()))<4:
-                line_bot_api.push_message(line_user_id, TextSendMessage("送られたメッセージが空白区切りで4個以下であるため、登録を行うことができませんでした。"))
-            else:
-                username = x[0]
-                groupname = x[1]
-                place = x[2]
-                teacher = x[3]
-                line_bot_api.push_message(line_user_id, TextSendMessage(f"username:{username},groupname:{groupname},place:{place},teacher:{teacher}として登録したよ"))
-        line_bot_api.push_message(line_user_id, TextSendMessage("こんにちは、"+username+"さん。"))
-        line_bot_api.push_message(line_user_id, TextSendMessage("URLを発行します。"))
-        mode = "活動開始"
-        no = "いいえ"
-        
-        line_bot_api.push_message(line_user_id, TextSendMessage(f"https://docs.google.com/forms/d/e/1FAIpQLSfKyju5yd5Fw08TiEo6Bwe3IO3HTM1gjngvWASKtMR8FpKtuA/viewform?usp=pp_url&entry.998530319={tourl(groupname)}&entry.273281967=2024-02-09&entry.1630282032={tourl(username)}&entry.449921247={tourl(mode)}&entry.1302965683=15:10&entry.2092899857={tourl(place)}&entry.358162710={tourl(teacher)}&entry.841836120={tourl(no)}&entry.1958684523=17:30"))
         
     # LINE Webhook サーバーへ HTTP レスポンスを返す
     return 'ok'
